d60be31532fc9dde3d2278d25f6c3eed9781b365
jjm2473 <1129525450@qq.com>
2020-03-17 12:57:03 +0800

    build script.

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
-VERSION=
+VERSION=-7.1.1
 
 echo './proc
+./cache
+./config
 ./sbin
 ./oem
+./storage
 ./system
 ./sys
+./mnt
 ./dev
+./acct
 ./data' | xargs -n1 sh -c 'mkdir -p android/$0; chmod 755 android/$0'
 
 chmod 750 android/sbin
